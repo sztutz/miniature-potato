4bee94430f619de90c1c80608245517614687baa
sztutz <emailofstuart@gmail.com>
2022-08-30 10:33:30 +0800

    Project created

---
+++ StudentScoreDB/src/studentscoredb/StudentScoreDB.java
@@ -0,0 +1,28 @@
+package studentscoredb;
+
+// Stuart Anderson, 30056472, 30/08/2022
+// OOP2, AT1-Activity 3
+// Student Score Database
+// A Java program that manages students’ scores in database is required. The program will read and
+// update records in the database, and show them on the screen. Do the task below:
+// o Design a database table that contains two columns, subject and score.
+// o Your program, when it begins, will create the database/table and records in MySQL.
+//   Name your database as student_management and name your table as
+//    student_score.
+//   If a database with the same name already exists in MySQL, it must be deleted before
+//    database creation.
+//   Add a few records in the table, e.g. English:95, Math:98, Science:89
+//   Provide your sql of database/table creation in a text file. Name this file as
+//    student_management.sql.
+// o Demonstrate reading the records from the table and show them on the screen.
+// o Demonstrate updating the records of the table and show them on the screen.
+// o Use the commit() method for database transaction integrity.
+// o Save your main class file as StudentScoreDB.java.
+        
+public class StudentScoreDB {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
